8009eaa21447c4ee88baaf71f61225fcae5c4ab3
zly <zly@iMac.local>
2016-05-03 10:38:03 +0800

    代码优化

--- WebRequest/Business/ThirdPartyLogin.m
+++ WebRequest/Business/ThirdPartyLogin.m
@@ -29,15 +29,6 @@
     //保存回调
     self.successBlock = successBlock;
     
-    //传入的参数
-    NSString* str = nil;
-    NSMutableDictionary *parameters = [[NSMutableDictionary alloc]init];
-    [parameters setObject:m_strOpenID forKey:@"openId"];
-    str = [NSString stringWithFormat:@"%d",m_nOpenType];
-    [parameters setObject:str forKey:@"openType"];
-    [parameters setObject:m_strUserName forKey:@"nickname"];
-    [parameters setObject:m_strUserImgUrl forKey:@"headimgurl"];
-    
     AppDelegate *app = [[UIApplication sharedApplication]delegate];
     //发送请求
     [app.m_ProManager.m_RequestManager ThirdPartyLogin:m_nOpenType OpenID:m_strOpenID UserName:m_strUserName UserImgUrl:m_strUserImgUrl success:^(id responseObject)
